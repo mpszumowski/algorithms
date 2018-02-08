a008ec44daca539899aab27fb252a064de6be337
Maciej Szumowski <mpszumowski@gmail.com>
2018-02-08 14:39:18 +0100

    frogriver

---
+++ various/FrogRiver.py
@@ -0,0 +1,18 @@
+"""
+Find missing element in array from 1 to N. Copyright by Codility
+https://app.codility.com/programmers/lessons/4-counting_elements/
+"""
+
+
+def solution(X, A):
+    path = [0] * X
+    steps = X
+    index = 0
+    for n in A:
+        if path[n-1] == 0:
+            path[n-1] += 1
+            steps -= 1
+            if steps == 0:
+                return index
+        index += 1
+    return -1
